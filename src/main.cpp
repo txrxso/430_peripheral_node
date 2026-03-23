a507250d3210ff3e2d63a33d5b90be8ae0ab0ee1
txrxso <teresaqwu@gmail.com>
2026-03-22 22:24:12 -0700

    final version of aq code

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ Sends HEARTBEAT_RESPONSE.
 #define DEBUG_MODE 1
 #define SENSOR_MOCK 0 // set to 1 to use mock sensor readings, set to 0 to use real sensor readings from SoundSensor class
 #define ENABLE_ACK 1 
-#define ALERT_INJECTION 1 // set to 1 to randomly inject alerts for testing, set to 0 for normal operation
+#define ALERT_INJECTION 0 // set to 1 to randomly inject alerts for testing, set to 0 for normal operation
 #define INJECTION_INTERVAL_MS 30000 // 30 seconds 
 
 // --- ALERTING THRESHOLDS --- 
